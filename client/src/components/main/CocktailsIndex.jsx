1fce5b6d500b3bb54194c2f1f9ae43a93d9e4dfc
Charles Muldoon <charm3728@gmail.com>
2020-06-21 22:49:05 -0700

    fix navbar, further stylized cards, added hover ingredients

--- client/src/components/main/CocktailsIndex.jsx
+++ client/src/components/main/CocktailsIndex.jsx
@@ -4,17 +4,18 @@ import Item from "./Item";
 const CocktailsIndex = ({mustHave, using, favorites, cocktails, favoritesPage}) => {
   return (
     <div className="drinkSection">
-      {cocktails.map((drink) => (
-          <Item
-            mustHave={mustHave}
-            favorites={favorites}
-            using={using}
-            key={drink.name}
-            favoritesPage={favoritesPage}
-            drink={drink}
-          />
-      ))}
-    </div>
+        {cocktails.map((drink) => (
+            <Item
+              mustHave={mustHave}
+              favorites={favorites}
+              using={using}
+              key={drink.name}
+              favoritesPage={favoritesPage}
+              drink={drink}
+            />
+        ))}
+      </div>
+
   );
 }
 
